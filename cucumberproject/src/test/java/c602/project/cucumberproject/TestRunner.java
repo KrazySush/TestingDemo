df189d0ccdd9a9e58f23b1cca1823cafd7abbfad
Sushant <Sushant@Sushant-LAP>
2017-10-15 23:02:57 +0530

    Formatted TestRunner class

--- cucumberproject/src/test/java/c602/project/cucumberproject/TestRunner.java
+++ cucumberproject/src/test/java/c602/project/cucumberproject/TestRunner.java
@@ -10,18 +10,16 @@ import com.cucumber.listener.Reporter;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-
 @RunWith(Cucumber.class)
-@CucumberOptions(
-		features="features",
-		glue={"c602.project.cucumberproject"},
-		plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/output/report.html"})
+@CucumberOptions(features = "features", glue = { "c602.project.cucumberproject" }, plugin = {
+		"com.cucumber.listener.ExtentCucumberFormatter:target/output/report.html" })
+
 public class TestRunner {
 	@AfterClass
-    public static void teardown() {
-        Reporter.loadXMLConfig(new File("extent-config.xml"));
-        Reporter.setSystemInfo("user", System.getProperty("user.name"));
-        Reporter.setSystemInfo("os", System.getProperty("os.name"));
-        Reporter.setTestRunnerOutput("This is Sample test runner output message");
-    }
+	public static void teardown() {
+		Reporter.loadXMLConfig(new File("extent-config.xml"));
+		Reporter.setSystemInfo("user", System.getProperty("user.name"));
+		Reporter.setSystemInfo("os", System.getProperty("os.name"));
+		Reporter.setTestRunnerOutput("This is Sample test runner output message");
+	}
 }
